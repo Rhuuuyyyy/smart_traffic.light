ceed911022dee97281868576bb472d0e3f1378d4
Rhyan Rocha <157857050+Rhuuuyyyy@users.noreply.github.com>
2025-06-03 11:36:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,45 +9,109 @@ basic_time = 15
 LED1_PIN_NUM = 13
 LED2_PIN_NUM = 14
 LED3_PIN_NUM = 12
+PIR1_PIN_NUM = 27
+PIR2_PIN_NUM = 26
 SWITCH_PIN_NUM = 2
 
 # Set the LED's output
 led1 = machine.Pin(LED1_PIN_NUM, machine.Pin.OUT)
 led2 = machine.Pin(LED2_PIN_NUM, machine.Pin.OUT)
 led3 = machine.Pin(LED3_PIN_NUM, machine.Pin.OUT)
+pir1 = machine.Pin(PIR1_PIN_NUM, machine.Pin.IN)
+pir2 = machine.Pin(PIR2_PIN_NUM, machine.Pin.IN)
 
 # Set the interruptor's function
 interruptor = machine.Pin(SWITCH_PIN_NUM, machine.Pin.IN)
 
 # Traffic light's mechanic 
-count = 0
-status_interruptor = interruptor.value()
 
-while True:
+def int_status():
     if status_interruptor == 1:
         led1.off()
         led2.off()
         led3.off()
-        count = 0
-        while count == 0:
-            led1.on()
-            time.sleep(basic_time)
+
+status_interruptor = interruptor.value()
+
+red_time = 15
+green_time = 15
+count = 0
+
+def int_status():
+
+    if interruptor.value() == 0:
+        led1.off()
+        led2.off()
+        led3.off()
+
+while True:
+
+    if interruptor.value() == 1:
+        if count == 0:
+            red_time_atual = red_time
+            while red_time_atual > 0:
+                if interruptor.value() == 0:
+                    count = 4
+                    break
+                led1.on()
+                time.sleep(1)
+                red_time_atual -= 1
+
+                if pir1.value() == 1 and pir2.value() == 0:
+                    red_time_atual += 5
+                elif pir1.value() == 0 and pir2.value() == 1:
+                    red_time_atual -= 5
+                elif pir1.value() == 0 and pir2.value() == 0:
+                    red_time_atual -= 1
+                elif pir1.value() == 1 and pir2.value() == 1:
+                    red_time_atual -= 1
+
             led1.off()
             count += 1
-        count = 1
-        if count == 1:
-            while count == 1:
+
+        elif count == 1:
+            if interruptor.value() == 0:
+                count = 4
+            else:
                 led2.on()
                 time.sleep(3)
                 led2.off()
                 count += 1
-        count = 2
-        if count == 2:
-            while count == 2:
+
+        elif count == 2:
+            green_time_atual = green_time
+            while green_time_atual > 0:
+                if interruptor.value() == 0:
+                    count = 4
+                    break
                 led3.on()
-                time.sleep(basic_time)
-                led3.off()
-                count += 1
+                time.sleep(1)
+                green_time_atual -= 1
+
+                if pir1.value() == 1 and pir2.value() == 0:
+                    green_time_atual -= 5
+                elif pir1.value() == 0 and pir2.value() == 1:
+                    green_time_atual += 2.5
+                elif pir1.value() == 0 and pir2.value() == 0:
+                    green_time_atual -= 1
+                elif pir1.value() == 1 and pir2.value() == 1:
+                    green_time_atual -= 1 
+
+            led3.off()
+            count += 1
+
+        elif count == 3:
+            if interruptor.value() == 0:
+                count = 4
+            else:
+                led2.on()
+                time.sleep(3)
+                led2.off()
+                count = 0
+
+        elif count == 4:
+            int_status()
+            if interruptor.value() == 1:
+                count = 0 
     else:
-        count = 0
-    time.sleep(0.1)
+        int_status()
